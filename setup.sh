8cf5e4796826f09da7a0d4effd6d4e2610effc07
ankitsharma1191 <ankitsharma.1191@gmail.com>
2020-10-16 00:59:22 +0530

    4

--- setup.sh
+++ setup.sh
@@ -1,10 +1,20 @@
 ## Author: Ankit Sharma
 ##Date: 10-6-2020
 ## this script will automatically setup your jenkins  machine 
-yum install epel-release -y
-yum install ansible -y 
-yum install git -y 
-git clone https://github.com/ankitsharma1191/jenkins
-cd jenkins
-ansible-playbook jenkins.yml
+
+yum update -y 
+if [ $? -eq 0 ]
+	then
+	yum install epel-release -y 
+		if [ $? -eq 0 ]
+			then
+			yum install ansible -y && yum install git -y 
+		 		if [ $? -eq 0 ]
+					then 
+					 git clone https://github.com/ankitsharma1191/jenkins
+					 cd jenkins
+					 ansible-playbook jenkins.yml
+				fi
+		fi
+fi
 
